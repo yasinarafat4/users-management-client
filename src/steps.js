6052ef24ec4cab00c68872ef9a3a8dfc70bf1af6
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-07 22:01:24 +0600

    getting users data from server

---
+++ src/steps.js
@@ -0,0 +1,8 @@
+/** To send data to the server
+ 
+ 1. create a post API on the server side
+ 2. client side snd data via post.
+ 3. set fetch method inside the fetch options
+4. options will have three things: method, headers, body
+5. On the server side, don't forget to use express.json() middleware
+ */
